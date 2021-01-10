d5f53b37f81a5188e212f6bb137cd4ba8e64ec1c
SommerKim <74261427+SommerKim@users.noreply.github.com>
2021-01-10 13:17:50 -0500

    Initial commit

---
+++ PyBank/main.py
@@ -0,0 +1,24 @@
+import os
+import csv
+
+budget_data = os.path.join('.', 'Resources', 'budget_data.csv')
+
+print(budget_data)
+
+with open(budget_data, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    header = next(csv_reader)
+
+
+
+    #find the total in the profit/losses column [1]
+    total_months = 0
+    
+
+    for row in csv_reader:
+        total_months += 1
+        
+    print("Financial Analysis")
+    print("------------------")
+    print(f"Total Months: {total_months}")
